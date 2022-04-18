70cdf236f9764666cc1d3cf0357690729af489cf
Debasish <debasish@Debasishs-MacBook-Air.local>
2022-04-18 19:37:22 +0530

    initial commit 2

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,6 @@
 'use strict';
 
-module.exports.generateRandomNumber = event => {
-  const randomNumber = parseInt(Math.random() * 100);
-  console.log("The random generated integer is: ", randomNumber);
-  return randomNumber;
+module.exports.hello = event => {
+  console.log("Hello world");
+  return "Hello world";
 };
